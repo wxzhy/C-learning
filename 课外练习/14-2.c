4f7e8cb35b8644c32801f57a0d68f5fb4b520da4
Tom Zhu <tomzhu2022@outlook.com>
2023-05-30 20:15:09 +0800

    Fix a mistake.

--- 课外练习/14-2.c
+++ 课外练习/14-2.c
@@ -8,11 +8,11 @@ int main() {
 	fprintf(fp,"---------------------------\n");
 	printf("---------------------------\n");
 	for(int i=1; i<=100; i++) {
-		long double t,u;
+		double t,u;
 		t=i*powl(i,2.0);
 		u=sqrtl(i);
-		printf("%6.0d%8.0Lf%9.3Lf\n",i,t,u);
-		fprintf(fp,"%6.0d%8.0Lf%9.3Lf\n",i,t,u);
+		printf("%6.0d%8.0lf%9.3lf\n",i,t,u);
+		fprintf(fp,"%6.0d%8.0lf%9.3lf\n",i,t,u);
 	}
 	fclose(fp);
 	return 0;
